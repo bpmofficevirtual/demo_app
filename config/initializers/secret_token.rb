69296cec8839e9937c2b2d2734541128a1da6feb
cristiano <bpmofficevirtual@gmail.com>
2011-11-15 15:14:00 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'eb810c72fa37cafe2931444c2405f4ca72c988a6d6d5210c6e0f4a1ee7d2b5fdb5e90240a0d5b74cd301372535e0b8f40f5927d80e8e2e964c4997038f87a10b'
